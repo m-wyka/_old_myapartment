153b001647d3fab176e53df2e2529f623a7a3a18
Mateusz Wyka <m.wyka@o2.pl>
2024-12-13 15:40:59 +0100

    Add frontend dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  i18n: {
+    strategy: "no_prefix",
+    locales: ["pl"],
+    defaultLocale: "pl",
+    lazy: true,
+    vueI18n: "./i18n/i18n.config.ts",
+    detectBrowserLanguage: false,
+    experimental: {
+      localeDetector: "locale-detector.ts",
+    },
+  },
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.BASE_URL,
+    },
+  },
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern", // For remove warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.
+        },
+      },
+    },
+  },
+});
